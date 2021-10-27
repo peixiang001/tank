0ece256c94d98a1b02c21c9f3087837203207ae9
lisi <1948179477@qq.com>
2021-10-27 17:13:03 +0800

    放弃T.class

--- src/com/px/tank/T.java
+++ src/com/px/tank/T.java
@@ -6,23 +6,6 @@ import java.awt.event.WindowEvent;
 
 public class T {
     public static void main(String[] args) {
-        //Frame图形窗口
-        Frame f = new Frame();
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("tank war");
-        //true设置可见
-        f.setVisible(true);
-
-        //设置退出
-        f.addWindowFocusListener(new WindowAdapter() {
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
-
+        TankFrame tankFrame = new TankFrame();
     }
 }
