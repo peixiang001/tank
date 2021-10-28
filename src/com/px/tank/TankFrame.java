1bf1ce2bedce6fc7372c3851b313f36a7625d811
lisi <1948179477@qq.com>
2021-10-28 10:30:40 +0800

    设置按键方向状态

--- src/com/px/tank/TankFrame.java
+++ src/com/px/tank/TankFrame.java
@@ -23,7 +23,10 @@ public class TankFrame extends Frame {
         //true设置可见
         setVisible(true);
 
-        //设置退出,图形右上角差号关闭
+        //监听键盘操作类
+        this.addKeyListener(new MyKeyListener());
+
+        //设置退出监听,图形右上角差号关闭
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -37,16 +40,16 @@ public class TankFrame extends Frame {
     //窗口重置(关闭再打开)时自动调用，Graphics类似画笔，可画图
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
         //设置位置和大小
         g.fillRect(x,y,40,40);
-        x +=10;
-        y += 10;
+
     }
 
 
     //键盘监听处理类
     class MyKeyListener extends KeyAdapter {
+        //用boolean值代表键被按下true，反之抬起false,
+        // 从而可以判断按键的上下左右的状态，通过状态再计算tank的移动位置
         boolean bL = false;
         boolean bU = false;
         boolean bR = false;
@@ -55,13 +58,58 @@ public class TankFrame extends Frame {
         //按下调用
         @Override
         public void keyPressed(KeyEvent e) {
-           //repaint();//会调用paint
+              int key = e.getKeyCode();
+              //判断按的是上下左右那个键
+              switch (key) {
+                  case KeyEvent.VK_LEFT:
+                      bL = true;
+                      x -=10;
+                      break;
+                  case KeyEvent.VK_UP:
+                      bU = true;
+                      y -= 10;
+                      break;
+                  case KeyEvent.VK_RIGHT:
+                      bR = true;
+                      x += 10;
+                      break;
+                  case KeyEvent.VK_DOWN:
+                      bD = true;
+                      y += 10;
+                      break;
+                  default:
+                      System.out.println("default");
+                      break;
+              }
         }
 
         //抬起调用
         @Override
         public void keyReleased(KeyEvent e) {
-            super.keyReleased(e);
+
+            int key = e.getKeyCode();
+            //判断按的是上下左右那个键
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+
+                    break;
+                default:
+
+                    break;
+            }
         }
     }
 
